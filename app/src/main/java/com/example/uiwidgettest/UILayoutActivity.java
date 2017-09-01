00ad86e7ebe42b5f8c64957481a2a96be68f1510
sunjianmcg <sjmcg@163.com>
2017-09-01 16:09:34 +0800

    基础布局练习内容

---
+++ app/src/main/java/com/example/uiwidgettest/UILayoutActivity.java
@@ -0,0 +1,14 @@
+package com.example.uiwidgettest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class UILayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.uilayout_layout);
+    }
+}
